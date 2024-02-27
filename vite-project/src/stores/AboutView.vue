fd4928ac0e87ef316a1d29c9464d51d9e8d92abf
SpinlyBinly <jane.laduca@gmail.com>
2024-02-26 21:02:17 -0500

    commit 9

---
+++ vite-project/src/stores/AboutView.vue
@@ -0,0 +1,83 @@
+<!-- <DestCard
+  v-for="destination in destinations"
+  :key="destination.name"
+ -->
+  import { ref } from 'vue'
+export const food = ref([
+  {
+    name: "Apples",
+    cost: 1.99,
+    category: "Produce",
+    imgs: [""],
+  },
+  {
+    name: "Milk",
+    cost: 2.49,
+    category: "Dairy",
+  },
+  {
+    name: "Bread",
+    cost: 1.99,
+    category: "Bakery",
+  },
+  {
+    name: "Ground Beef",
+    cost: 4.99,
+    category: "Meat",
+  },
+  {
+    name: "Cereal",
+    cost: 3.49,
+    category: "Breakfast",
+  },
+  {
+    name: "Bananas",
+    cost: 0.79,
+    category: "Produce",
+  },
+  {
+    name: "Eggs",
+    cost: 2.69,
+    category: "Dairy",
+  },
+  {
+    name: "Chicken",
+    cost: 7.99,
+    category: "Meat",
+  },
+  {
+    name: "Broccoli",
+    cost: 1.29,
+    category: "Produce",
+  },
+  {
+    name: "Yogurt",
+    cost: 1.49,
+    category: "Dairy",
+  },
+  {
+    name: "Chips",
+    cost: 3.49,
+    category: "Snacks",
+  },
+  {
+    name: "Soda",
+    cost: 1.99,
+    category: "Beverages",
+  },
+  {
+    name: "Cheese",
+    cost: 3.49,
+    category: "Dairy",
+  },
+  {
+    name: "Ice Cream",
+    cost: 4.49,
+    category: "Frozen Foods",
+  },
+  {
+    name: "",
+    cost: "",
+    category: "",
+  },
+]);
