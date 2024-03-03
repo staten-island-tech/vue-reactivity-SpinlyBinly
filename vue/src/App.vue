a377cd7c0c0284876387fd6567f9a45f50d8a487
SpinlyBinly <jane.laduca@gmail.com>
2024-03-02 20:39:54 -0500

    commit 2.2

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <RouterView />
+<HomeView/> 
 </template>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,32 +1,33 @@
 <template>
+<div class="header"></div>
   <div class="container">
+    
     <div class="card" v-for="product in supermarket">
       <h3>{{ product.name }}</h3>
       <h3>{{ product.cost }}</h3>
       <h3>{{ product.category }}</h3>
-      <button @click="add(product)">Add to Cart!</button>
+      <button class="buttons" @click="add(product)">Add to Cart!</button>
     </div>
   </div>
-  <div class="cart">
+<div class="cart">
     <div class="cartItems" v-for="item in cart">
       <h3>{{ item.name }}</h3>
       <h3>{{ item.cost }}</h3>
       <h3>{{ item.category }}</h3>
       <h3>{{ item.amount }}</h3>
-      <button @click="remove(item)">Remove From Cart</button>
+      <button class="buttons" @click="remove(item)">Remove From Cart</button>
     </div>
-  </div>
-</template>
+    </div>
+</template>  
 
-<script setup>
+<script setup> ;
 import { reactive } from "vue";
 const cart = reactive([]);
 const supermarket = [
   {
     name: "Apples",
-    cost: 1.99,
+    cost: 1.99,   
     category: "Produce",
-    imgs: [""],
   },
   {
     name: "Milk",
@@ -99,7 +100,7 @@ function add(food) {
     cart.push(food);
     food.amount = 1;
   } else {
-    food.amount += 1;
+    food.amount += 0;
   }
 }
 function remove(item) {
@@ -109,21 +110,54 @@ function remove(item) {
 </script>
 
 <style scoped>
+.buttons{
+border: 0;
+}
+
+.header{
+  width: 69.5%;
+  height: 10vh;
+  background-color: #6b9080;
+}
+body { 
+    background-color: #eaf4f4;
+    font-family: Interstate, sans-serif;
+    font: 200;
+    font-size: x-large;
+}
 .card {
-  width: 22%;
+  width: 15%;
+  background-color: #cce3de;
+  border-radius: 3%;
+  margin: 1%;
+ text-align: center;
+
 }
 .container {
   display: flex;
   flex-wrap: wrap;
+  width: 82%;
 }
 .cart {
-  height: 100%; /* Full-height: remove this if you want "auto" height */
-  width: 160px; /* Set the width of the sidebar */
-  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-  z-index: 1; /* Stay on top */
-  top: 0; /* Stay at the top */
+  height: 100%; 
+  width: 30%; 
+  position: fixed; 
+  z-index: 1;
+  top: 0;
   right: 0;
-  overflow-x: hidden; /* Disable horizontal scroll */
+  overflow-x: hidden; 
   padding-top: 20px;
+    background-color: #a4c3b2;
+    text-align: center;
+}
+
+.cartItems {
+  margin: 20%;
+  margin-top: 5%;
+  flex-wrap: wrap;
+  align-items: center;
+  background-color: #eaf4f4;
+  border-radius: 2%;
 }
 </style>
+
